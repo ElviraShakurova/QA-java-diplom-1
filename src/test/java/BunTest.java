a8a6ea78c21b237017cb428cc346ff675af9c422
Elvira Shakurova <elenshakurova@gmail.com>
2023-12-19 14:22:46 +0300

    add change BunTest, IngredientTest

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,67 +1,71 @@
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
 public class BunTest {
 
+    private String bunName = "black bun";
+    private float bunPrice = 100f;
+    private String emptyBunName = "";
+    private String longBunName = "Очень очень преочень длинное длинное предлинное значение";
+    private String specialCharactersBunName = "Bun#$5%";
+    private float negativeBunPrice = -100f;
+    private float zeroBunPrice = 0f;
+    private float minimumPositiveBunPrice = 0.001f;
+    private float maximumPositiveBunPrice = 999f;
+
     @Test
     public void testGetName(){
-        Bun bun = new Bun("black bun", 100f);
-        assertEquals("black bun", bun.getName());
+        Bun bun = new Bun(bunName, bunPrice);
+        assertEquals(bunName, bun.getName());
     }
 
     @Test
     public void testGetPrice(){
-        Bun bun = new Bun("black bun", 100f);
-        assertEquals(100f, bun.getPrice(), 0);
+        Bun bun = new Bun(bunName, bunPrice);
+        assertEquals(bunPrice, bun.getPrice(), 0);
     }
 
     @Test
     public void testGetEmptyName() {
-        Bun bun = new Bun("", 100f);
-        assertEquals("", bun.getName());
+        Bun bun = new Bun(emptyBunName, bunPrice);
+        assertEquals(emptyBunName, bun.getName());
     }
 
     @Test
     public void testGetLongName() {
-        Bun bun = new Bun("Очень очень преочень длинное длинное предлинное значение", 100f);
-        assertEquals("Очень очень преочень длинное длинное предлинное значение", bun.getName());
+        Bun bun = new Bun(longBunName, bunPrice);
+        assertEquals(longBunName, bun.getName());
     }
 
     @Test
     public void testGetNameWithSpecialCharacters() {
-        Bun bun = new Bun("Bun#$5%", 100f);
-        assertEquals("Bun#$5%", bun.getName());
+        Bun bun = new Bun(specialCharactersBunName, bunPrice);
+        assertEquals(specialCharactersBunName, bun.getName());
     }
 
     @Test
     public void testGetNegativePrice() {
-        Bun bun = new Bun("black bun", -100f);
-        assertEquals(-100f, bun.getPrice(), 0);
+        Bun bun = new Bun(bunName, negativeBunPrice);
+        assertEquals(negativeBunPrice, bun.getPrice(), 0);
     }
 
     @Test
     public void testGetPriceReturnZero() {
-        Bun bun = new Bun("black bun", 0f);
-        assertEquals(0, bun.getPrice(), 0);
+        Bun bun = new Bun(bunName, zeroBunPrice);
+        assertEquals(zeroBunPrice, bun.getPrice(), 0);
     }
 
     @Test
     public void testGetPriceReturnsMinimumPositiveValue() {
-        Bun bun = new Bun("black bun", 0.001f);
-        assertEquals(0.001f, bun.getPrice(), 0);
+        Bun bun = new Bun(bunName, minimumPositiveBunPrice);
+        assertEquals(minimumPositiveBunPrice, bun.getPrice(), 0);
     }
 
     @Test
     public void testGetPriceReturnsMaximumPositiveValue() {
-        Bun bun = new Bun("black bun", 999f);
-        assertEquals(999f, bun.getPrice(), 0);
+        Bun bun = new Bun(bunName, maximumPositiveBunPrice);
+        assertEquals(maximumPositiveBunPrice, bun.getPrice(), 0);
     }
 }
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -12,18 +12,21 @@ import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IngredientTest {
+    private float ingredientPrice = 100f;
+    private String ingredientName = "hot sauce";
+
     @Test
     public void testGetPrice(){
         Ingredient ingredient = mock(Ingredient.class);
-        Mockito.when(ingredient.getPrice()).thenReturn(100f);
-        assertEquals(100f, ingredient.getPrice(), 0);
+        Mockito.when(ingredient.getPrice()).thenReturn(ingredientPrice);
+        assertEquals(ingredientPrice, ingredient.getPrice(), 0);
     }
 
     @Test
     public void testGetName(){
         Ingredient ingredient = mock(Ingredient.class);
-        Mockito.when(ingredient.getName()).thenReturn("hot sauce");
-        assertEquals("hot sauce", ingredient.getName());
+        Mockito.when(ingredient.getName()).thenReturn(ingredientName);
+        assertEquals(ingredientName, ingredient.getName());
     }
 
     @Test
